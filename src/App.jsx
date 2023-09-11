e765733968c885d6b854ea01d8f3153616ba9df3
JoseZR <josezerrod@hotmail.com>
2023-09-11 00:34:11 -0600

    cambio en footer

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export function App () {
       </div>
       <footer>
         <Container>
-          <div class='d-flex bd-highlight mb-4'>
+          <div class='d-flex bd-highlight'>
             <div class=' bd-highlight' style={{ width: '200px' }}><img src='/foro-electromovilidad/igeco_bn.webp' alt='igeco' className='w-100' /></div>
             <div class=' bd-highlight' style={{ width: '200px' }}><img src='/foro-electromovilidad/deutsche_messe_bn.webp' alt='igeco' className='w-100' /></div>
             <div class=' ms-auto' style={{ width: '200px' }}><img src='/foro-electromovilidad/logo-gto.webp' alt='gto-200años' className='w-100' /></div>
