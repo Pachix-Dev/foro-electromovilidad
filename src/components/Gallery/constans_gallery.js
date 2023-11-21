043cf861a5f17eec57b1e59c945f8eea5ab2a3a1
JoseZR <josezerrod@hotmail.com>
2023-11-21 16:51:54 -0600

    se actualiza ruta de imagenes

--- src/components/Gallery/constans_gallery.js
+++ src/components/Gallery/constans_gallery.js
@@ -1,20 +1,19 @@
-/* eslint-disable indent */
+
 const gallery2023 = [
-  '/foro-electromovilidad/gallery/electromovilidad_1.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_2.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_3.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_4.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_5.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_6.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_7.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_8.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_9.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_10.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_11.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_12.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_13.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_14.webp',
-  '/foro-electromovilidad/gallery/electromovilidad_15.webp'
+  '/foro-electromovilidad/gallery/foro_electromovilidad_1.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_2.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_3.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_4.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_5.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_6.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_7.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_8.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_9.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_10.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_11.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_12.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_13.webp',
+  '/foro-electromovilidad/gallery/foro_electromovilidad_14.webp'
 ]
 
 export { gallery2023 }
